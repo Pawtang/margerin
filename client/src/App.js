ad6e0fbcd7150fd224b389634ccaa2a8addf83ee
Pawtang <ben.dibuz@gmail.com>
2021-12-14 15:55:53 -0600

    Add some homepage features, but need to redo layout

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,18 @@ function App() {
     <Fragment>
       <div className="container-fluid background">
         <div className="container">
+          <div className="row justify-content-end">
+            <div className="col-auto">
+              <button className="btn btn-light pull-right">Log In</button>
+            </div>
+            <div className="col-auto">
+              <button className="btn btn-outline-light pull-right">
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="container titlebox">
           <h1>margerin</h1>
           <p className="white">Take the guesswork out of product costing</p>
           <button className="btn btn-outline-light">Try a Demo</button>
