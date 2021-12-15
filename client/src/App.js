b888b199ccad03ea6bca057adffeb200b526ab6e
Ben Dibuz <ben.dibuz@gmail.com>
2021-12-14 21:22:42 -0600

    Styling landing

--- client/src/App.js
+++ client/src/App.js
@@ -7,26 +7,52 @@ function App() {
   return (
     <Fragment>
       <div className="container-fluid background">
-        <div className="container">
-          <div className="row justify-content-end">
-            <div className="col-auto">
-              <button className="btn btn-light pull-right">Log In</button>
-            </div>
-            <div className="col-auto">
-              <button className="btn btn-outline-light pull-right">
-                Sign Up
-              </button>
-            </div>
+        <nav className="navbar bg-transparent">
+          <div className="container justify-content-end mt-4">
+            <button class="btn btn-light me-md-2" type="button">
+              Log In
+            </button>
+            <button class="btn btn-outline-light" type="button">
+              Sign Up
+            </button>
+          </div>
+        </nav>
+
+        <div className="container-fluid d-flex align-items-center hero">
+          <div className="container titlebox">
+            <h1 className="title">margerin</h1>
+            <h1 className="white display-6">
+              Take the guesswork out of product costing
+            </h1>
+            <button className="btn btn-lg btn-outline-light mt-2">
+              Try a Demo
+            </button>
           </div>
         </div>
-        <div className="container titlebox">
-          <h1>margerin</h1>
-          <p className="white">Take the guesswork out of product costing</p>
-          <button className="btn btn-outline-light">Try a Demo</button>
-        </div>
       </div>
+
       <div className="container content">
         <h2>Lightweight and Affordable Tool for Small Businesses</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+          molestie in sapien id pretium. Aenean et varius augue. Sed varius sem
+          a metus ultrices varius. Donec egestas mauris ut ligula vestibulum
+          euismod quis eget lectus. Quisque ullamcorper, diam non mattis.
+        </p>
+        <div className="row text-center justify-content-between">
+          <div className="col-3 shadow align-items my-5">
+            <i class="bi bi-clipboard-data big-icon"></i>
+            <p className="text-center">Record recipes data</p>
+          </div>
+          <div className="col-3 shadow my-5">
+            <i class="bi bi-calculator big-icon"></i>
+            <p className="text-center">Automatically calculate cost of goods</p>
+          </div>
+          <div className="col-3 shadow my-5">
+            <i class="bi bi-cash-coin big-icon"></i>
+            <p className="text-center">Set prices with confidence</p>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
