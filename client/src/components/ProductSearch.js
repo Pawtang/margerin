4ccce13cd78958c895c2402afab3e78ad1942b1e
Ben Dibuz <ben.dibuz@gmail.com>
2022-01-03 12:11:08 -0600

    search semi-separated

--- client/src/components/ProductSearch.js
+++ client/src/components/ProductSearch.js
@@ -1,10 +1,10 @@
 import { displayProduct } from "../middleware/DashboardUtils";
 import { React, useState } from "react";
+import { getProducts } from "../middleware/DashboardUtils";
 
-export const ProductSearch = () => {
+export const ProductSearch = (props) => {
   const [search, setSearch] = useState("");
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [displayedProduct, setDisplayedProduct] = useState({});
+  const { products, setDisplayedProduct } = props;
 
   return (
     <div className="col-5 col-md-3 border-end">
@@ -49,37 +49,44 @@ export const ProductSearch = () => {
           </button>
         </div>
         {/* --------------------------- Render Product List -------------------------- */}
-        {filteredProducts.map((product) => (
-          <div className="row my-1" key={product.product_id}>
-            <button
-              className="btn btn-outline-primary"
-              onClick={async () => {
-                const productData = await displayProduct(product.product_id);
-                setDisplayedProduct(productData);
-                setSearch("");
-              }}
-            >
-              <div className="row">
-                <div className="col-10 text-start">{product.product_name}</div>
-                <div className="col">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-arrow-right-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-                    />
-                  </svg>
+
+        {products
+          .filter((product) =>
+            product.product_name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((product) => (
+            <div className="row my-1" key={product.product_id}>
+              <button
+                className="btn btn-outline-primary"
+                onClick={async () => {
+                  const productData = await displayProduct(product.product_id);
+                  setDisplayedProduct(productData);
+                  setSearch("");
+                }}
+              >
+                <div className="row">
+                  <div className="col-10 text-start">
+                    {product.product_name}
+                  </div>
+                  <div className="col">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-right-circle"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                      />
+                    </svg>
+                  </div>
                 </div>
-              </div>
-            </button>
-          </div>
-        ))}
+              </button>
+            </div>
+          ))}
         {/* ----------------------------- End Render List ---------------------------- */}
       </div>
     </div>
